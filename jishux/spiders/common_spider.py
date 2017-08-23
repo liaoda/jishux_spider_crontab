581f6415106ce052c15d691901ae99e382ad163b
yaochao <yaochao365@qq.com>
2017-08-24 00:04:08 +0800

    fix

--- jishux/spiders/common_spider.py
+++ jishux/spiders/common_spider.py
@@ -72,11 +72,11 @@ class CommonSpider(scrapy.Spider):
             request.meta['conf'] = conf
             yield request
 
-        # 翻页
-        # request = next_page(callback=self.parse, response=response, conf=conf, first_url=first_url,
-        #                     latest_url=latest_url, post_type=post_type)
-        # if request:
-        #     yield request
+        翻页
+        request = next_page(callback=self.parse, response=response, conf=conf, first_url=first_url,
+                            latest_url=latest_url, post_type=post_type)
+        if request:
+            yield request
 
     def parse_post(self, response):
         crawl_time = None
