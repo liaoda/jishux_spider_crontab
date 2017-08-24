262bd5c2ea5ff941cde441dd44c26bcd7750c66f
TrancyDeng <dengqiangxi@icloud.com>
2017-08-24 12:06:44 +0800

    fix timestamp

--- jishux/misc/time_formater.py
+++ jishux/misc/time_formater.py
@@ -5,13 +5,17 @@ import re
 import dateparser
 import time
 import random
+
 time_re = re.compile('[年月日\s]')
 
 
 def generate_timestamp(post_time):
     now = int(time.time())
-    time_stamp = int(dateparser.parse(time_re.sub(' ', post_time)).timestamp())
+    date_time = dateparser.parse(time_re.sub(' ', post_time))
+    if date_time:
+        time_stamp = int(date_time.timestamp())
+    else:
+        return now
     if time_stamp > now:
         time_stamp = now - random.randint(0, 600)
     return time_stamp
-
