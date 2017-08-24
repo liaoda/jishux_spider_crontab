a78e7db89b679d651e4183627384674f3c9855dc
TrancyDeng <dengqiangxi@icloud.com>
2017-08-24 10:48:14 +0800

    fix article type column

--- jishux/pipelines.py
+++ jishux/pipelines.py
@@ -147,12 +147,13 @@ class JishuxMysqlPipeline(object):
         content = item['content_html'].replace("'", "\\'") if item['content_html'] else ''
         title = item['post_title'] if item['post_title'] else ''
         source = item['cn_name']
+        article_type ='p' if len(item['image_urls']) >0 else ''
         author = '技术栈' if not item['author'] else item['author']
         litpic = item['litpic'] if item['litpic']else ''
         type_id = get_post_type_id(item['post_type'])
         crawl_time = str(item['crawl_time'])
         sql_insert_meta = 'INSERT INTO dede_archives (typeid, sortrank, flag, ismake, channel, title, writer, source, pubdate, senddate, mid, keywords, description, dutyadmin,voteid,litpic,click) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "'"+%s+"'", "%s", "%s","%s","%s","%s")' % (
-            type_id, crawl_time, 'p', -1, 1, title, author, source, crawl_time, crawl_time,
+            type_id, crawl_time, article_type, -1, 1, title, author, source, crawl_time, crawl_time,
             1, keywords, description, 1, 0, litpic, random.randint(5000, 10000))
         self.cursor.execute(sql_insert_meta)
         sql_last_id = 'SELECT LAST_INSERT_ID()'
