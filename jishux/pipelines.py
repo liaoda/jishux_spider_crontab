4a7ee4aa4de3a5da52e67f2deabb6f29b94966b7
yaochao <yaochao365@qq.com>
2017-09-04 15:33:24 +0800

    delete print(), add logger

--- jishux/pipelines.py
+++ jishux/pipelines.py
@@ -121,7 +121,6 @@ class JISHUXFilePipeline(FilesPipeline):
 
     def item_completed(self, results, item, info):
         item['litpic'] = ''
-        print(results)
         for x in results:
             if x[0]:
                 # 上传
@@ -209,7 +208,7 @@ class JishuxMysqlPipeline(object):
         aid = a['LAST_INSERT_ID()']
         sql_insert_content = 'INSERT INTO dede_addonarticle (aid, typeid, body, userip) VALUES("%s","%s","%s","%s")' % (
             aid, type_id, content, '127.0.0.1')
-        print(sql_insert_content)
+        # print(sql_insert_content)
         self.cursor.execute(sql_insert_content)
         sql_insert_arctiny = 'INSERT INTO dede_arctiny (id, typeid, channel, senddate, sortrank,mid) VALUES ("%s", "%s", "%s", "%s", "%s", "%s")' % (
             aid, type_id, 1, crawl_time, crawl_time, 1)
@@ -236,7 +235,7 @@ class JishuxMysqlPipeline(object):
             # 插入tag到taglist
             sql_insert_tag_list = 'INSERT INTO dede_taglist (tid, aid, arcrank, typeid, tag) VALUES ("%s", "%s", "%s", "%s", "%s")' % (
                 str(tid), str(aid), 0, str(type_id), key)
-            print(sql_insert_tag_list)
+            # print(sql_insert_tag_list)
             self.cursor.execute(sql_insert_tag_list)
 
         self.connection.commit()
